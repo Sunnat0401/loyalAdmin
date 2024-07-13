d6decf59ea4b38ea3564e0335c45e677224a0ec6
Sunnat <sunnattoxtaqulov4444@gmail.com>
2024-07-13 08:31:16 +0500

    delte pagenotfound

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import LoginPage from './Pages/LoginPage/LoginPage.jsx';
-import HomePages from './Pages/HomePages/HomePages.jsx';
-import PageNotFound from './Components/PageNotFound/PageNotFound.jsx';
+// import React from 'react'
+import LoginPage from './Pages/LoginPage/LoginPage.jsx'
+import HomePages from './Pages/HomePages/HomePages.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/home' element={<HomePages />} />
-        <Route path='*' element={<PageNotFound />} />
+        <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/home' element={<HomePages/>}/>  
       </Routes>
+ 
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
